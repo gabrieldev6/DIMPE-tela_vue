7b0e8d84fbf2d5065c910015c2f214f2b5f56608
gabrieldev6 <gabriel.christ.silva@gmail.com>
2024-07-05 14:10:19 -0300

    remoção comentario

--- src/core/Detector.ts
+++ src/core/Detector.ts
@@ -1,5 +1,4 @@
 import * as tf from '@tensorflow/tfjs'
-// import '@tensorflow/tfjs-backend-webgl'
 import { non_max_suppression } from './nonMaxSuppression'
 import labels from '../assets/labels.json'
 
